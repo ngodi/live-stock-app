e6a24189fae59a7d9515c800bd9db9dbb08f43ce
ngodi <albertngdi@gmail.com>
2020-10-03 16:41:08 +0100

    deploy settings

--- src/Stocks.js
+++ src/Stocks.js
@@ -1,42 +1,39 @@
 import React, { useState, useEffect } from 'react';
-
 import { updateData, formatChartData } from './utils/utils';
 import StockTable from './components/StockTable';
 import MyChart from './components/MyChart';
-
 import './Stocks.scss';
 
-let response = "";
-let chartData = [ ["Element", "Price", { role: "style" }] ,["", 0, ""] ] ;
+let response = '';
+let chartData = [['Element', 'Price', { role: 'style' }] ,['', 0, '']] ;
 const Stocks = () => {
-     const [ticker, setTicker] = useState({});
-     const [color, setColor] = useState({});
-         
-    useEffect(() => {
-      const ws = new WebSocket('ws://stocks.mnet.website');
-      ws.onopen = () => {
-        console.log("connected");
-      };
-      ws.onmessage = (event) => {
+  const [ticker, setTicker] = useState({});
+  const [color, setColor] = useState({});
+  useEffect(() => {
+    const ws = new WebSocket('ws://stocks.mnet.website');
+    ws.onopen = () => {
+      console.log('connected');
+    };
+    ws.onmessage = (event) => {
       response = JSON.parse(event.data);
-        updateData(response, ticker, setTicker, setColor);
-        chartData = formatChartData(ticker, chartData);
-       };
-      ws.onclose = () => {
-        new WebSocket('ws://stocks.mnet.website');
-      };
+      updateData(response, ticker, setTicker, setColor);
+      chartData = formatChartData(ticker, chartData);
+    };
+    ws.onclose = () => {
+      new WebSocket('ws://stocks.mnet.website');
+    };
   
-      return () => {
-        ws.close();
-      };
-    },[ticker]);
+    return () => {
+      ws.close();
+    };
+  },[ticker]);
 
- return(
+  return(
    <div className="stock">
       <MyChart data={chartData}/>
-     { (!response)? "....fetching data-" :  <StockTable ticker={ticker} color={color} />}
+     { (!response)? '....fetching data-' :  <StockTable ticker={ticker} color={color} />}
    </div>
- )
-  }
+  )
+}
   
 export default Stocks;
