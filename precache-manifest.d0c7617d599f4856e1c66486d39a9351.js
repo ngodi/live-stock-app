07f49b6b97c153377f033286dd700f369e25c60f
ngodi <albertngdi@gmail.com>
2020-10-03 07:43:40 +0100

    Updates

---
+++ precache-manifest.d0c7617d599f4856e1c66486d39a9351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6ca865421ad0de5f8a2c7bbe924278",
+    "url": "/live-stock-app/index.html"
+  },
+  {
+    "revision": "a18deede161d2b17ca9d",
+    "url": "/live-stock-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ede9d873b067b4c2a1c2",
+    "url": "/live-stock-app/static/js/2.de8f5438.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live-stock-app/static/js/2.de8f5438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18deede161d2b17ca9d",
+    "url": "/live-stock-app/static/js/main.695e2c7b.chunk.js"
+  },
+  {
+    "revision": "2faed586bdae134d2f91",
+    "url": "/live-stock-app/static/js/runtime-main.b96234b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/live-stock-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
