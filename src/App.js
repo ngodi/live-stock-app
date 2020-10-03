91e9803534eb53a32604a4e535ffb543e04a219b
ngodi <albertngdi@gmail.com>
2020-10-03 08:22:00 +0100

    deploy automation settings via travis-ci

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-       <h2>Live Stocks  - {(new Date).toDateString()}</h2>
+      <h2>
+        Live Stocks  -
+        {(new Date()).toDateString()}
+      </h2>
     </div>
   );
 }
